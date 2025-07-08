59094d31dc92d01d8b2e32d49ee66a528589a2a0
Suruchicodes <suruchik661@gmail.com>
2025-07-08 16:23:11 +0530

    Update project files and configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Changed from absolute URL to root-relative
+  base: "/suruchi_portfolio/", // Changed from absolute URL to root-relative
   css: {
     modules: {
       localsConvention: "camelCase",
